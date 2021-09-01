afc57b6ad06441d04a291fb32a0c50c7956bbe60
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2021-09-01 14:20:21 -0500

    Update buildaudio.sh

--- buildaudio.sh
+++ buildaudio.sh
@@ -12,15 +12,15 @@ alerturl=$( printf "%s\n" "$url$clear$county_name" | sed 's/ /%20/g' )
 wget -U Mozilla -O "/tmp/clear.wav" $clearurl
 wget -U Mozilla -O "/tmp/alert.wav" $alerturl
 
-if test -f "as96Z.wav"; then
+if test -f $(pwd)"/as96Z.wav"; then
     mv asn96Z.wav asn96Z.wav.old
-    sox asn96Z-tweet.wav /tmp/clear.wav asn96Z.wav
+    sox $(pwd)/asn96Z-tweet.wav /tmp/clear.wav $(pwd)/asn96Z.wav
 else
     echo "asn96Z.wav not detected!"
 fi
-if test -f "as97.wav"; then
+if test -f $(pwd)"/as97.wav"; then
     mv asn97.wav asn97.wav.old
-    sox asn97-tweet.wav /tmp/alert.wav asn97.wav
+    sox $(pwd)/asn97-tweet.wav /tmp/alert.wav $(pwd)/asn97.wav
 else
     echo "asn97.wav not detected!"
 fi
