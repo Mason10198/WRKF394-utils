1a2ba58dddc9dbe31de736c025b88f4dc932bd82
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2021-09-01 16:33:57 -0500

    Update buildaudio.sh

--- buildaudio.sh
+++ buildaudio.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-
 . /home/repeater/WRKF394-utils/params.conf
 
 url="https://api.voicerss.org/?key="$voicerss_key"&hl=en-us&src="
@@ -18,10 +13,10 @@ alerturl=$( printf "%s\n" "$url$clear$county_name" | sed 's/ /%20/g' )
 wget -U Mozilla -O "/tmp/clear.wav" $clearurl
 wget -U Mozilla -O "/tmp/alert.wav" $alerturl
 
-cp ./AUTOSKY/SOUNDS/asn96Z.wav ./AUTOSKY/SOUNDS/asn96Z.wav.old
-cp ./AUTOSKY/SOUNDS/asn97.wav ./AUTOSKY/SOUNDS/asn97.wav.old
+cp $base_dir/AUTOSKY/SOUNDS/asn96Z.wav $base_dir/AUTOSKY/SOUNDS/asn96Z.wav.old
+cp $base_dir/AUTOSKY/SOUNDS/asn97.wav $base_dir/AUTOSKY/SOUNDS/asn97.wav.old
 
 echo "Compiling audio files..."
-sox ./AUTOSKY/SOUNDS/asn96Z-tweet.wav /tmp/clear.wav ./AUTOSKY/SOUNDS/asn96Z.wav
-sox ./AUTOSKY/SOUNDS/asn97-tweet.wav /tmp/alert.wav /AUTOSKY/SOUNDS/asn97.wav
+sox $base_dir/AUTOSKY/SOUNDS/asn96Z-tweet.wav /tmp/clear.wav $base_dir/AUTOSKY/SOUNDS/asn96Z.wav
+sox $base_dir/AUTOSKY/SOUNDS/asn97-tweet.wav /tmp/alert.wav $base_dir/AUTOSKY/SOUNDS/asn97.wav
 echo "Done."
