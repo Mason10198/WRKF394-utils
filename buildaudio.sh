6d9afc88ae8874bd5714dcaebab8051810dfa159
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2021-09-01 15:58:46 -0500

    Update buildaudio.sh

--- buildaudio.sh
+++ buildaudio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./params.conf
-cd $(pwd)/AUTOSKY/SOUNDS
+cd $(dirname "$0")/AUTOSKY/SOUNDS
 url="https://api.voicerss.org/?key="$voicerss_key"&hl=en-us&src="
 
 clear="The national weather service has no current alerts, watches, or warnings for "
@@ -12,10 +12,10 @@ alerturl=$( printf "%s\n" "$url$clear$county_name" | sed 's/ /%20/g' )
 wget -U Mozilla -O "/tmp/clear.wav" $clearurl
 wget -U Mozilla -O "/tmp/alert.wav" $alerturl
 
-cp $(pwd)/asn96Z.wav $(pwd)/asn96Z.wav.old
-cp $(pwd)/asn97.wav $(pwd)/asn97.wav.old
+cp $(dirname "$0")/asn96Z.wav $(dirname "$0")/asn96Z.wav.old
+cp $(dirname "$0")/asn97.wav $(dirname "$0")/asn97.wav.old
 
 echo "Compiling audio files..."
-sox $(pwd)/asn96Z-tweet.wav /tmp/clear.wav $(pwd)/asn96Z.wav
-sox $(pwd)/asn97-tweet.wav /tmp/alert.wav $(pwd)/asn97.wav
+sox $(dirname "$0")/asn96Z-tweet.wav /tmp/clear.wav $(dirname "$0")/asn96Z.wav
+sox $(dirname "$0")/asn97-tweet.wav /tmp/alert.wav $(dirname "$0")/asn97.wav
 echo "Done."
