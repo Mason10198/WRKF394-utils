2bd4c8204496fdee0fcdf235257562edf2c98418
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2021-09-01 15:56:28 -0500

    Update buildaudio.sh

--- buildaudio.sh
+++ buildaudio.sh
@@ -3,7 +3,7 @@
 cd $(pwd)/AUTOSKY/SOUNDS
 url="https://api.voicerss.org/?key="$voicerss_key"&hl=en-us&src="
 
-clear="The national weather sevice has no current alerts, watches, or warnings for "
+clear="The national weather service has no current alerts, watches, or warnings for "
 alert="Updated weather information for "
 
 clearurl=$( printf "%s\n" "$url$clear$county_name" | sed 's/ /%20/g' )
@@ -18,3 +18,4 @@ cp $(pwd)/asn97.wav $(pwd)/asn97.wav.old
 echo "Compiling audio files..."
 sox $(pwd)/asn96Z-tweet.wav /tmp/clear.wav $(pwd)/asn96Z.wav
 sox $(pwd)/asn97-tweet.wav /tmp/alert.wav $(pwd)/asn97.wav
+echo "Done."
