d041313b96888972d8b0a09521b0e4d443893e31
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2021-09-01 18:28:04 -0500

    update

--- install.sh
+++ install.sh
@@ -7,8 +7,24 @@ install_utils () {
     sudo chmod -R +x /home/repeater/WRKF394-utils/
 
     echo "Building audio files..."
-    sudo /home/repeater/WRKF394-utils/build_audio.sh
+    url="https://api.voicerss.org/?key="$voicerss_key"&hl=en-us&src="
 
+    clear="The national weather service has no current alerts, watches, or warnings for "
+    alert="Updated weather information for "
+
+    clearurl=$( printf "%s\n" "$url$clear$county_name" | sed 's/ /%20/g' )
+    alerturl=$( printf "%s\n" "$url$alert$county_name" | sed 's/ /%20/g' )
+
+    wget -U Mozilla -O "/tmp/clear.wav" $clearurl
+    wget -U Mozilla -O "/tmp/alert.wav" $alerturl
+
+    cp $base_dir/AUTOSKY/SOUNDS/asn96Z.wav $base_dir/AUTOSKY/SOUNDS/asn96Z.wav.old
+    cp $base_dir/AUTOSKY/SOUNDS/asn97.wav $base_dir/AUTOSKY/SOUNDS/asn97.wav.old
+
+    echo "Compiling audio files..."
+    sox $base_dir/AUTOSKY/SOUNDS/asn96Z-tweet.wav /tmp/clear.wav $base_dir/AUTOSKY/SOUNDS/asn96Z.wav
+    sox $base_dir/AUTOSKY/SOUNDS/asn97-tweet.wav /tmp/alert.wav $base_dir/AUTOSKY/SOUNDS/asn97.wav
+    
     echo "Building local node name file..."
     wget -q -U Mozilla -O "/home/repeater/WRKF394-utils/nodenames/"$node_number".wav" "https://api.voicerss.org/?key="$voicerss_key"&hl=en-us&src="$repeater_name
 
