954958e46de538e6b6d69180336f4d5dce3594a9
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2021-10-11 06:23:26 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,6 @@ git clone http://github.com/mason10198/WRKF394-utils /tmp/WRKF394-utils/
 sudo mv /tmp/WRKF394-utils/nodenames/* /home/repeater/WRKF394-utils/nodenames/
 sudo mv /tmp/WRKF394-utils/privatenodes.txt /home/repeater/WRKF394-utils/
 echo "Moving node name files..."
-sudo rm -rf /var/lib/asterisk/sounds/rpt/nodenames
 sudo cp -avr /tmp/WRKF394-utils/nodenames /var/lib/asterisk/sounds/rpt/nodenames
 echo "Moving privatenodes.txt..."
 sudo cp /tmp/WRKF394-utils/privatenodes.txt /var/www/html/allmon2/privatenodes.txt
