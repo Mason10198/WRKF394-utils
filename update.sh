0d8ef517e60b269a1e44e5d8114819ec0120bc71
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2021-10-11 06:46:29 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,13 +2,13 @@
 echo "Updating nodename audio files and privatenodes.txt..."
 sudo rm -rf /tmp/WRKF394-utils/
 git clone http://github.com/mason10198/WRKF394-utils /tmp/WRKF394-utils/
-sudo cp /tmp/WRKF394-utils/nodenames/* /home/repeater/WRKF394-utils/nodenames/
-sudo cp /tmp/WRKF394-utils/privatenodes.txt /home/repeater/WRKF394-utils/
+sudo mv /tmp/WRKF394-utils/nodenames/* /home/repeater/WRKF394-utils/nodenames/
+sudo mv /tmp/WRKF394-utils/privatenodes.txt /home/repeater/WRKF394-utils/
 echo "Moving node name files..."
-sudo cp -avr /tmp/WRKF394-utils/nodenames /var/lib/asterisk/sounds/rpt/nodenames
+sudo cp -avr /home/repeater/WRKF394-utils/nodenames /var/lib/asterisk/sounds/rpt/nodenames
 echo "Moving privatenodes.txt..."
-sudo cp /tmp/WRKF394-utils/privatenodes.txt /var/www/html/allmon2/privatenodes.txt
-sudo cp /tmp/WRKF394-utils/privatenodes.txt /var/www/html/supermon/privatenodes.txt
+sudo cp /home/repeater/WRKF394-utils/privatenodes.txt /var/www/html/allmon2/privatenodes.txt
+sudo cp /home/repeater/WRKF394-utils/privatenodes.txt /var/www/html/supermon/privatenodes.txt
 echo "Updating node info via astdb.php..."
 sudo chmod +x /var/www/html/allmon2/astdb.php
 sudo chmod +x /var/www/html/supermon/astdb.php
