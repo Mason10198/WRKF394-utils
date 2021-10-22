e3fa175b04c4544dd1e4923654d3a5ceb2bffec4
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2021-10-22 17:34:06 -0500

    Update say_wx_time.sh

--- say_wx_time.sh
+++ say_wx_time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . /home/repeater/WRKF394-utils/params.conf
-/home/repeater/WRKF394-utils/say_time.pl $zip_code $node_number 1
+/home/repeater/WRKF394-utils/say_time.pl $node_number 1
 /home/repeater/WRKF394-utils/save_daycast.sh -f
 sox -V /tmp/current-time.gsm -r 8000 -c 1 -t ul /tmp/currenttime.ul
 sox /tmp/currenttime.ul /home/repeater/WRKF394-utils/silence.ul /tmp/daycast.ul /tmp/combined_time_forecast.ul
